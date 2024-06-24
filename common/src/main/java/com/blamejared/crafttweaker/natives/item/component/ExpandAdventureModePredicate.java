10e6928a4f3ac5f9bc783bbeba844c03d2139159
Jared <jaredlll08@gmail.com>
2024-06-23 18:07:07 -0600

    expose adventuremodepredicate

--- common/src/main/java/com/blamejared/crafttweaker/natives/item/component/ExpandAdventureModePredicate.java
+++ common/src/main/java/com/blamejared/crafttweaker/natives/item/component/ExpandAdventureModePredicate.java
@@ -1,15 +1,17 @@
 package com.blamejared.crafttweaker.natives.item.component;
 
 import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import com.blamejared.crafttweaker.api.tag.type.KnownTag;
 import com.blamejared.crafttweaker_annotations.annotations.Document;
 import com.blamejared.crafttweaker_annotations.annotations.NativeTypeRegistration;
+import com.google.common.collect.Lists;
 import net.minecraft.advancements.critereon.BlockPredicate;
-import net.minecraft.network.chat.Component;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.AdventureModePredicate;
+import net.minecraft.world.level.block.Block;
 import org.openzen.zencode.java.ZenCodeType;
 
 import java.util.List;
-import java.util.function.Consumer;
 
 @ZenRegister
 @Document("vanilla/api/item/component/AdventureModePredicate")
@@ -18,16 +20,36 @@ public class ExpandAdventureModePredicate {
     
     @ZenCodeType.StaticExpansionMethod
     public static AdventureModePredicate of(List<BlockPredicate> predicates, boolean showInTooltip) {
+        
         return new AdventureModePredicate(predicates, showInTooltip);
     }
     
-    @ZenCodeType.Method
-    public static AdventureModePredicate withTooltip(AdventureModePredicate internal, boolean value) {
-        return internal.withTooltip(value);
+    @ZenCodeType.StaticExpansionMethod
+    public static AdventureModePredicate ofBlocks(List<Block> predicates, boolean showInTooltip) {
+        
+        return new AdventureModePredicate(Lists.transform(predicates, input -> BlockPredicate.Builder.block()
+                .of(input)
+                .build()), showInTooltip);
+    }
+    
+    @ZenCodeType.StaticExpansionMethod
+    public static AdventureModePredicate ofTags(List<KnownTag<Block>> predicates, boolean showInTooltip) {
+        
+        return new AdventureModePredicate(Lists.transform(predicates, input -> BlockPredicate.Builder.block()
+                .of(input.<TagKey<Block>> getTagKey())
+                .build()), showInTooltip);
+    }
+    
+    @ZenCodeType.Getter("showInTooltip")
+    public static boolean showInTooltip(AdventureModePredicate internal) {
+        
+        return internal.showInTooltip();
     }
     
     @ZenCodeType.Method
-    public static void addToTooltip(AdventureModePredicate internal, Consumer<Component> consumer) {
-        internal.addToTooltip(consumer);
+    public static AdventureModePredicate withTooltip(AdventureModePredicate internal, boolean tooltip) {
+        
+        return internal.withTooltip(tooltip);
     }
+    
 }
