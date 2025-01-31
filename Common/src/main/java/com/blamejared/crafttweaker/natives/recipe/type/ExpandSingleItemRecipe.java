aabd2aadbbd0d3d363b2f0f23802e8fa760ea9af
Jared <jaredlll08@gmail.com>
2025-01-31 13:12:24 -0700

    Expose missing SingleItemRecipe class. Close #1792

---
+++ Common/src/main/java/com/blamejared/crafttweaker/natives/recipe/type/ExpandSingleItemRecipe.java
@@ -0,0 +1,14 @@
+package com.blamejared.crafttweaker.natives.recipe.type;
+
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import com.blamejared.crafttweaker_annotations.annotations.Document;
+import com.blamejared.crafttweaker_annotations.annotations.NativeTypeRegistration;
+import net.minecraft.world.item.crafting.SingleItemRecipe;
+import net.minecraft.world.item.crafting.StonecutterRecipe;
+
+@ZenRegister
+@Document("vanilla/api/recipe/type/SingleItemRecipe")
+@NativeTypeRegistration(value = SingleItemRecipe.class, zenCodeName = "crafttweaker.api.recipe.type.SingleItemRecipe")
+public class ExpandSingleItemRecipe {
+
+}
